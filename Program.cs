93f5aebe3fc8a4e4e58380a08028fad48341caa2
Hans <cagaanan.hans@gmail.com>
2026-04-13 01:55:41 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int Stock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"ID Number:{Id} | {Name} - {Price} (Stock: {Stock})");
+    }
+
+    public double GetItemTotal(int quantity)
+    {
+        return Price * quantity;
+    }
+
+    public bool HasEnoughStock(int quantity)
+    {
+        return quantity <= Stock;
+    }
+
+    public void DeductStock(int quantity)
+    {
+        Stock -= quantity;
+    }
+}
