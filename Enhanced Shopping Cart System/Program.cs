56d6b79c1c93c93881565c1f12f4624b023d7bdf
haczkk <cagaanan.hans@gmail.com>
2026-04-30 14:49:13 +0800

    Made the Product menu and cart static so it can be accessible by all methods.

--- Enhanced Shopping Cart System/Program.cs
+++ Enhanced Shopping Cart System/Program.cs
@@ -4,20 +4,31 @@ namespace ShoppingCartSystem
 {
     class Program
     {
+        const int MAX_CART          = 10;
+        const int MAX_ORDERS        = 50;
+        const int LOW_STOCK_LEVEL   = 5;
+        const double DISCOUNT_MIN   = 5000;
+        const double DISCOUNT_RATE  = 0.10;
+
+        static Product[] menu        = new Product[10];
+        static CartItem[] cart       = new CartItem[MAX_CART];
+        static int cartCount         = 0;
+
+        static Receipt[] orderHistory = new Receipt[MAX_ORDERS];
+        static int orderCount         = 0;
+        
         static void Main()
         {
-            Product[] menu = new Product[7];
-            menu[0] = new Product(1, "Chair", 1_500.00, 5);
-            menu[1] = new Product(2, "Table", 3_000.00, 3);
-            menu[2] = new Product(3, "Sofa", 10_000.00, 0);
-            menu[3] = new Product(4, "Bed", 15_000.00, 1);
-            menu[4] = new Product(5, "Bookshelf", 5_000.00, 4);
-            menu[5] = new Product(6, "Dresser", 7_500.00, 2);
-            menu[6] = new Product(7, "Lamp", 2_000.00, 6);
-    
-            int maxCart = 5;
-            CartItem[] cart =  new CartItem[maxCart];
-            int cartCount = 0;
+            menu[0] = new Product(1,  "Chair",       1_500.00, 10, "Furniture");
+            menu[1] = new Product(2,  "Table",       3_000.00, 10, "Furniture");
+            menu[2] = new Product(3,  "Sofa",       10_000.00, 10, "Furniture");
+            menu[3] = new Product(4,  "Bed",        15_000.00, 10, "Furniture");
+            menu[4] = new Product(5,  "Bookshelf",   5_000.00, 10, "Furniture");
+            menu[5] = new Product(6,  "Dresser",     7_500.00, 10, "Appliances");
+            menu[6] = new Product(7,  "Ref",        25_000.00, 10, "Appliances");
+            menu[7] = new Product(8,  "Microwave",   8_000.00, 10, "Appliances");
+            menu[8] = new Product(9,  "Aircon",     35_000.00, 10, "Appliances");
+            menu[9] = new Product(10, "Wall Clock",    800.00, 10, "Home Decor");
     
             string continueShopping = "Y";
     
