28a74742b7b9489e00ba369699f9228ee218730d
haczkk <cagaanan.hans@gmail.com>
2026-05-07 00:34:55 +0800

    Replaced direct public fields with getters and setters.

--- Enhanced Shopping Cart System/CartItem.cs
+++ Enhanced Shopping Cart System/CartItem.cs
@@ -2,8 +2,8 @@ namespace ShoppingCartSystem
 {
     public class CartItem
     {
-        public Product Product;
-        public int Quantity;
+        public Product Product { get; private set; }
+        public int Quantity { get; set; }
 
         public CartItem(Product product, int quantity)
         {
